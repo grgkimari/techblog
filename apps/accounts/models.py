b3653601c3454c22157cdaab298e81ba98146906
Kimari <grgkimari@gmail.com>
2022-08-23 02:10:45 +0300

    added  login/register views

--- apps/accounts/models.py
+++ apps/accounts/models.py
@@ -4,3 +4,5 @@ from django.contrib.auth.models import User
 
 
 
+
+
--- techblog/urls.py
+++ techblog/urls.py
@@ -18,5 +18,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('apps.mainblog.urls'))
+    path('', include('apps.mainblog.urls')),
+    path('accounts/', include('django.contrib.auth.urls')),
+    path('accounts/', include('apps.accounts.urls')),
 ]
