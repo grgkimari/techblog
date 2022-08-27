55d5e222f56f55929f5502e5795e6716c35f1156
Kimari <grgkimari@gmail.com>
2022-08-28 02:26:08 +0300

    added categories drop-down menus

--- apps/mainblog/views.py
+++ apps/mainblog/views.py
@@ -11,29 +11,65 @@ class HomeView(ListView):
     model = Post
     template_name = 'homepage.html'
 
+    def get_context_data(self, **kwargs: any) :
+        category_menu = Category.objects.all()
+        context = super(HomeView, self).get_context_data(**kwargs)
+        context['category_menu'] = category_menu
+        return context
+
 class ArticleView(DetailView):
     model = Post
     template_name = 'article_details.html'
+
+    def get_context_data(self, **kwargs: any) :
+        category_menu = Category.objects.all()
+        context = super(ArticleView, self).get_context_data(**kwargs)
+        context['category_menu'] = category_menu
+        return context
 
 class AddPostView(CreateView):
     model = Post
     form_class = PostForm
     template_name = 'add_post.html'
 
+    def get_context_data(self, **kwargs: any) :
+        category_menu = Category.objects.all()
+        context = super(AddPostView, self).get_context_data(**kwargs)
+        context['category_menu'] = category_menu
+        return context
+
 class EditPostView(UpdateView):
     model = Post
     template_name = 'edit_post.html'
     fields = ['title', 'body', 'category']
+
+    def get_context_data(self, **kwargs: any) :
+        category_menu = Category.objects.all()
+        context = super(EditPostView, self).get_context_data(**kwargs)
+        context['category_menu'] = category_menu
+        return context
 
 class DeletePostView(DeleteView):
     model = Post
     template_name = 'delete_post.html'
     success_url = reverse_lazy('homepage')
 
+    def get_context_data(self, **kwargs: any) :
+        category_menu = Category.objects.all()
+        context = super(DeletePostView, self).get_context_data(**kwargs)
+        context['category_menu'] = category_menu
+        return context
+
 class AddCategoryView(CreateView):
     model = Category
     fields  = '__all__'
     template_name = 'add_category.html'
+
+    def get_context_data(self, **kwargs: any) :
+        category_menu = Category.objects.all()
+        context = super(AddCategoryView, self).get_context_data(**kwargs)
+        context['category_menu'] = category_menu
+        return context
 
 def CategoryView(request, category):
   
